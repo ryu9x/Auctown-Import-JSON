6e9aa17e59389d6319e8d4a246c2136bc0a97697
ryu9x <Nguyenduy9x@gmail.com>
2019-07-23 00:42:17 +0900

    asd
    ads

--- custom.js
+++ custom.js
@@ -35,11 +35,15 @@ function fetchData(){
 	//Mo ta
 	$.get(fetchJSONData.descriptionModule.descriptionUrl,function(responseHTML){
 		var customDesc = "";
-		customDesc += $("#summernote_desc").val($('#summernote_temp_desc').val().replace("#desc-heading",fetchJSONData.titleModule.subject));
-		customDesc += $("#summernote_desc").val($('#summernote_temp_desc').val().replace("#desc-body",responseHTML));
-		customDesc += $('#summernote_temp_shipping').val();
+
+
+		customDesc += $('#summernote_temp_desc').val().replace("#desc-heading",fetchJSONData.titleModule.subject).replace("#desc-body",responseHTML).replace(".shipping-schedule",$("#summernote_temp_shipping_time").val());
+
+		customDesc += $('#summernote_temp_shipping').val().replace(".shipping-schedule",$("#summernote_temp_shipping_time").val());
 		customDesc += $('#summernote_temp_payment').val();
 		customDesc += $('#summernote_temp_note').val();
+		$("#summernote_desc").val(customDesc);
+		
 
 		$("#easyWebEditor_description_copy_iframe").contents().find("body").html(customDesc);
 	});
@@ -86,8 +90,9 @@ function fetchData(){
 
 function loadSummernote(){
 	try{
-		$('#summernote_temp_desc').val('<table width="100%" cellspacing="0" cellpadding="10" border="0"><tbody><tr><th><div id="desc-heading"><h3>#desc-heading</h3></div></th></tr><tr><td bgcolor="#60992e"><strong> <span style="COLOR: #FFFFFF;">出品物</span> </strong></td></tr><tr><td><div id="desc-note">#desc-note</div></td></tr><tr><td><div id="desc-body">#des-body</div></td></tr></tbody></table>');
-		$('#summernote_temp_shipping').val('<table width="100%" cellspacing="0" cellpadding="10" border="0"><tbody><tr><td bgcolor="#75a749"><strong><span style="COLOR: RGB(255,255,255);">発送</span></strong></td></tr><tr><td><p><span style="COLOR: #FF0000;">入金を確認してから １５～３０日で発送。</span></p><p><font face="Helvetica" color="#333333">※同梱を希望される場合</font></p><p><span style="COLOR: RGB(51,51,51);"><font face="Helvetica">同梱は可能ですが、送料は１点ずつの記載送料のお支払いをお願い致します。</font></span></p><p><span style="COLOR: RGB(51,51,51);">例：２点落札の場合&nbsp;</span><span style="COLOR: RGB(51,51,51);">&nbsp;</span></p><p><font face="Helvetica"><font color="#333333">１点目　</font><span style="COLOR: RGB(51,51,51);">落札金額＋送料　＝　A</span></font></p><p><font face="Helvetica" color="#333333">２点目　落札金額＋送料　＝　B</font></p><p><font face="Helvetica"><span style="COLOR: RGB(51,51,51);">&nbsp;</span><span style="COLOR: RGB(51,51,51);">&nbsp;A＋B　＝　お支払い金額</span></font></p><p><font face="Helvetica"><span style="COLOR: RGB(51,51,51);">※送料のご相談や交渉は、受け付けていませんのでご了承ください。&nbsp;</span><span style="COLOR: RGB(51,51,51);">&nbsp;</span></font></p></td></tr></tbody></table>');
+		$('#summernote_temp_shipping_time').val('<span style="COLOR: #FF0000;">入金を確認してから １５～３０日で発送。</span>');
+		$('#summernote_temp_desc').val('<table width="100%" cellspacing="0" cellpadding="10" border="0"><tbody><tr><th><div id="desc-heading"><h6>#desc-heading</h6></div></th></tr><tr><td bgcolor="#60992e"><strong> <span style="COLOR: #FFFFFF;">出品物</span> </strong></td></tr><tr><td><div id="note" class="shipping-schedule">.shipping-schedule</div></td></tr><tr><td><div id="desc-body">#desc-body</div></td></tr></tbody></table>');
+		$('#summernote_temp_shipping').val('<table width="100%" cellspacing="0" cellpadding="10" border="0"><tbody><tr><td bgcolor="#75a749"><strong><span style="COLOR: RGB(255,255,255);">発送</span></strong></td></tr><tr><td><p id="note" class="shipping-schedule">.shipping-schedule</p><p><font face="Helvetica" color="#333333">※同梱を希望される場合</font></p><p><span style="COLOR: RGB(51,51,51);"><font face="Helvetica">同梱は可能ですが、送料は１点ずつの記載送料のお支払いをお願い致します。</font></span></p><p><span style="COLOR: RGB(51,51,51);">例：２点落札の場合&nbsp;</span><span style="COLOR: RGB(51,51,51);">&nbsp;</span></p><p><font face="Helvetica"><font color="#333333">１点目　</font><span style="COLOR: RGB(51,51,51);">落札金額＋送料　＝　A</span></font></p><p><font face="Helvetica" color="#333333">２点目　落札金額＋送料　＝　B</font></p><p><font face="Helvetica"><span style="COLOR: RGB(51,51,51);">&nbsp;</span><span style="COLOR: RGB(51,51,51);">&nbsp;A＋B　＝　お支払い金額</span></font></p><p><font face="Helvetica"><span style="COLOR: RGB(51,51,51);">※送料のご相談や交渉は、受け付けていませんのでご了承ください。&nbsp;</span><span style="COLOR: RGB(51,51,51);">&nbsp;</span></font></p></td></tr></tbody></table>');
 		$('#summernote_temp_payment').val('<table width="100%" cellspacing="0" cellpadding="10" border="0"><tbody><tr><td bgcolor="#75a749"><strong><span style="COLOR: RGB(255,255,255);">支払方法</span></strong><br></td></tr><tr><td><ol><li>●　Yahoo!かんたん</li><li><span>●　</span>Yahoo!マネー/預金払い</li><li><span>●　</span>クレジットカード</li><li><span>●　</span>インターネットバンキング</li><li><span>●　</span>コンビニ支払い</li></ol><p><span style="COLOR: RGB(255,0,0);">　※直接の銀行振込は受け付けておりません。落札後はすべて、かんたん決済（銀行振込は不可、銀行振込以外の支払い方法を選択してください。）にてお支払いをお願いします。</span></p></td></tr></tbody></table>');
 		$('#summernote_temp_note').val('<table width="100%" cellspacing="0" cellpadding="10" border="0"><tbody><tr><td bgcolor="#75a749"><strong> <span style="COLOR: #FFFFFF;">備考</span> </strong></td></tr><tr><td><div><font size="3"><br></font></div><ol><li><font size="3">●　ご入金は落札後の3日以内にお願い致します。</font></li><li><font size="3">●　振込手数料は落札者様がご負担ください。</font></li><li><font size="3">●　落札後の返品、交換はお受けできません。</font></li><li><font size="3">●　故意による破損の返品、交換もお受けできません。</font></li><li><font size="3">●　商品を受け取る前に商品が紛失、受領されていない場合（40日以内にご連絡ください。ご連絡を受けた日から１週間以内にご返金致します</font>。）</li><li><font size="3">●　故意によるものではない破損の場合（3日以内にご連絡くだささい。ご返金または交換致します。）</font></li><li><font size="3">●　お使いのモニターによっては、実物との色合いが異なる場合がございます。ご了承ください。</font></li></ol></td></tr></tbody></table>');
 
